59ad4f7f11a800159e209bd6ba03bbc8c06c459c
Diego Müller <42013866+h4ns7@users.noreply.github.com>
2022-03-31 10:30:40 -0300

    up

--- exercicio/desafio/d005/script.js
+++ exercicio/desafio/d005/script.js
@@ -1,5 +1,18 @@
-i = 2
-for(var c = i ; c <= 10 ; c += c ){
-    console.log(`${c}`)
-
+function tabuada() {
+    let num = document.getElementById('nu1')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert('Por favor digite o numero')
+    }else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while( c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    } 
 }
